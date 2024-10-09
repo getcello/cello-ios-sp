69fbb73960d029b16d65081e4f35eca202d59fa0
Cello Dev <developer@cello.so>
2024-10-09 20:06:02 +0100

    Releasing 0.2.0

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "CelloSDK",
-      url: "https://github.com/getcello/cello-ios-sp/releases/download/0.1.1/CelloSDK.xcframework.zip",
-      checksum: "b86935c6c683046d6fe2993e4e860c1e178bb97f859e82ecd9a7672cae94e5e4")
+      url: "https://github.com/getcello/cello-ios-sp/releases/download/0.2.0/CelloSDK.xcframework.zip",
+      checksum: "0c34ddf55b7ed8ed1089456d4585fd84b74c578bd63bde7117f178cf2b27e679")
   ]
 )
